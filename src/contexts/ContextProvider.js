085984c3999e33dac094b56feacfe1791ebd66c0
adiletelf <adiletelf@gmail.com>
2022-07-22 15:38:14 +0600

    Create Navbar component

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -5,18 +5,26 @@ const StateContext = createContext();
 const initialState = {
   chat: false,
   cart: false,
-  useProfile: false,
+  userProfile: false,
   notification: false,
 };
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  const handleClick = (clicked) => {
+    setIsClicked({...initialState, [clicked]: true});
+  }
 
   return (
     <StateContext.Provider
       value={{
-        activeMenu,
-        setActiveMenu,
+        activeMenu, setActiveMenu,
+        isClicked, setIsClicked,
+        handleClick,
+        screenSize, setScreenSize,
       }}
     >
       {children}
