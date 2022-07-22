aa181136383dd719ddee12d7cb533394c209a8e0
adiletelf <adiletelf@gmail.com>
2022-07-22 14:56:03 +0600

    Create Sidebar component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers,
-  Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor
+  Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line
 } from './pages';
 import './App.css';
 
 const App = () => {
-  const activeMenu = false;
+  const activeMenu = true;
 
   return (
     <div>
