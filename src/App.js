50a64fb3d82b1cf4c172034843dcf9a056224e89
adiletelf <adiletelf@gmail.com>
2022-07-22 15:08:27 +0600

    Add ContextProvider

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers,
 } from './pages';
 import './App.css';
 
+import { useStateContext } from './contexts/ContextProvider';
+
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
